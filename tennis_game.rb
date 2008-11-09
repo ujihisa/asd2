37b88c247c1d3ad6e09ece286552714818fecb61
okkez <okkez000@gmail.com>
2008-11-09 18:03:12 +0900

    add tennis_game

---
+++ tennis_game.rb
@@ -0,0 +1,10 @@
+
+class TennisGame
+  def initialize(max_set_count)
+    @max_set_count = max_set_count
+  end
+
+  def score
+    [0, 0]
+  end
+end
---
+++ tennis_game_spec.rb
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+require 'rubygems'
+require 'spec'
+require 'tennis_game'
+
+describe "Tennis Game" do
+  describe "初期状態は" do
+    before do
+      @game = TennisGame.new(6)
+    end
+    it " 0 - 0 (ラブオール) であること" do
+      @game.score.should == [0, 0]
+    end
+  end
+end
