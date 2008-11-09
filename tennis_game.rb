f981936cec1c08a33442675ccea1c8bc63862a62
okkez <okkez000@gmail.com>
2008-11-10 01:19:51 +0900

    add TennisGame::Game examples

--- tennis_game.rb
+++ tennis_game.rb
@@ -1,10 +1,74 @@
 
-class TennisGame
-  def initialize(max_set_count)
-    @max_set_count = max_set_count
+module TennisGame
+  class Match
+    def initialize(player1, player2, max_set_count)
+      @player1 = player1
+      @player2 = player2
+      @max_set_count = max_set_count
+    end
   end
 
-  def score
-    [0, 0]
+  class Game
+    
+    
+    
+    def initialize(player1, player2)
+      @player1 = player1
+      @player2 = player2
+      @score = {
+        @player1 => 0,
+        @player2 => 0
+      }
+    end
+    # human readable
+    def score
+      calculate_score(@score[@player1], @score[@player2])
+    end
+
+    def raw_score
+      @score
+    end
+
+    def inspect
+      "#{@player1.name} : #{@score[@player1]}, #{@player2.name} : #{@score[@player2]}"
+    end
+
+    def add(player)
+      @score[player] += 1
+    end
+
+    def winner
+      case
+      when @score[@player1] == @score[@player2]
+        nil
+      when @score[@player1] >= 4
+        @player1
+      when @score[@player2] >= 4
+        @player2
+      else
+        nil
+      end
+    end
+
+    private
+    
+    def calculate_score(point1, point2)
+      case
+      when point1 >= 4 && point2 >= 4 && point1 == point2
+        :duce
+      when point1 >= 4 && point2 >= 4 && point1 > point2
+        :advantage
+      when point1 >= 4 && point2 >= 4 && point1 < point2
+        :advantage
+      else
+        [point1, point2]
+      end
+    end
+  end
+
+  class Player
+    def initialize(name)
+      @name = name
+    end
   end
 end
--- tennis_game_spec.rb
+++ tennis_game_spec.rb
@@ -3,13 +3,80 @@ require 'rubygems'
 require 'spec'
 require 'tennis_game'
 
-describe "Tennis Game" do
+describe "TennisGame::Game" do
   describe "初期状態" do
     before do
-      @game = TennisGame.new(6)
+      @player1 = TennisGame::Player.new('a')
+      @player2 = TennisGame::Player.new('b')
+      @game = TennisGame::Game.new(@player1, @player2)
     end
     it "は、 0 - 0 (ラブオール) であること" do
-      @game.score.should == [0, 0]
+      @game.raw_score.values.should == [0, 0]
+    end
+    it "は、勝者は未定であること" do
+      @game.winner.should == nil
+    end
+  end
+  describe "player1 が 1 ポイント取ったとき" do
+    before do
+      @player1 = TennisGame::Player.new('a')
+      @player2 = TennisGame::Player.new('b')
+      @game = TennisGame::Game.new(@player1, @player2)
+      @game.add(@player1)
+    end
+    it "は、1 - 0 (15 - 0) であること" do
+      @game.raw_score[@player1].should == 1
+      @game.raw_score[@player2].should == 0
+    end
+    it "は、勝者は未定であること" do
+      @game.winner.should == nil
+    end
+  end
+  describe "player1 が 4 ポイント取ったとき" do
+    before do
+      @player1 = TennisGame::Player.new('a')
+      @player2 = TennisGame::Player.new('b')
+      @game = TennisGame::Game.new(@player1, @player2)
+      4.times{ @game.add(@player1) }
+    end
+    it "は、 4 - 0 であること" do
+      @game.raw_score[@player1].should == 4
+      @game.raw_score[@player2].should == 0
+    end
+    it "は、player1 がこのゲームの勝者であること" do
+      @game.winner.should == @player1
+    end
+  end
+  describe "player2 が 4 ポイント取ったとき" do
+    before do
+      @player1 = TennisGame::Player.new('a')
+      @player2 = TennisGame::Player.new('b')
+      @game = TennisGame::Game.new(@player1, @player2)
+      4.times{ @game.add(@player2) }
+    end
+    it "は、 0 - 4 であること" do
+      @game.raw_score[@player1].should == 0
+      @game.raw_score[@player2].should == 4
+    end
+    it "は、player2 がこのゲームの勝者であること" do
+      @game.winner.should == @player2
+    end
+  end
+  describe "player1 と player2 が 4 ポイントずつ取ったとき" do
+    before do
+      @player1 = TennisGame::Player.new('a')
+      @player2 = TennisGame::Player.new('b')
+      @game = TennisGame::Game.new(@player1, @player2)
+      4.times{
+        @game.add(@player1)
+        @game.add(@player2)
+      }
+    end
+    it "は、ジュースであること" do
+      @game.score.should == :duce
+    end
+    it "は、勝者は未定であること" do
+      @game.winner.should == nil
     end
   end
 end
