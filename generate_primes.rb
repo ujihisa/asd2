941f038ca661f8fc37dc5d5864229a3e57c4c790
okkez <okkez000@gmail.com>
2008-11-09 16:48:32 +0900

    Fix: compat Ruby1.9

--- generate_primes.rb
+++ generate_primes.rb
@@ -1,5 +1,20 @@
+<<<<<<< Updated upstream:generate_primes.rb
 
 def generate_primes(num)
   # FIXME
   [num]
+=======
+if RUBY_VERSION >= '1.9.0'
+  require 'prime'
+
+  def generate_primes(num)
+    Prime.instance.take_while{|i| i <= num }
+  end
+else
+  require 'mathn'
+
+  def generate_primes(num)
+    Prime.new.take_while {|i| i <= num }
+  end
+>>>>>>> Stashed changes:generate_primes.rb
 end
