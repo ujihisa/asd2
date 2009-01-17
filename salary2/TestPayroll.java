0102cc6616d997a64817dd1447a872c5d7b4b218
ujihisa <ujihisa@gmail.com>
2009-01-17 16:16:37 +0900

    list 19-2 (+alpha)

---
+++ salary2/TestPayroll.java
@@ -0,0 +1,21 @@
+public class TestPayroll {
+  public void testAddSalariedEmployee() {
+    System.err.println("TestAddSalariedEmployee");
+    int empId = 1;
+    AddSalariedEployee t = new AddSalariedEmployee(empId, "Bob", "Home", 1000.00);
+    t.Execute();
+    Employee e = PayrollDatabase.GetEmployee(empId);
+    assertNotNull(e);
+    assertEquals("Bob", e.GetName());
+    PaymentClassification pc = e.GetClassification();
+    SalariedClassification sc = (SalariedClassification) pc;
+    assertNotNull(cs);
+    assertEquals(1000.00, sc.GetSalary());
+    PaymentSchedule ps = e.GetSchedule();
+    MonthlySchedule ms = (MonthlySchedule) ps;
+    assertNotNull(ms);
+    PaymentMethod pm = g.GetMethod();
+    HoleMethod hm = (HoldMehotd) pM;
+    assertNotNull(hm);
+  }
+}
